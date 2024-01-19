2c0c47297f2ee375c9c26ec4b5a3e3eb58706235
Johannes Kalma (werklaptop) <j.kalma@eu.aswatson.com>
2024-01-19 19:00:16 +0100

    documentation

--- src/org/asw/kafkafactory/ConsumerGeneric.java
+++ src/org/asw/kafkafactory/ConsumerGeneric.java
@@ -29,7 +29,7 @@ public class ConsumerGeneric<V> {
 
 	private Long startTime;
 	private Integer iterator = 0;
-	boolean doCommit;
+	private boolean doCommit;
 
 	/**
 	 * constructor<br>
@@ -47,7 +47,7 @@ public class ConsumerGeneric<V> {
 	/**
 	 * dataprocessor also prints the Values.
 	 * 
-	 * @return Consumer instance
+	 * @return This ConsumerGeneric (to allow chaining)
 	 */
 	public ConsumerGeneric<V> printValues() {
 		doPrintValues = true;
@@ -58,41 +58,45 @@ public class ConsumerGeneric<V> {
 	 * 1. when seek, then no timer (value=0), subscriber should then iterate exactly
 	 * once (while loop) 2. when subscriber, look eternaly (24 hours).
 	 * 
-	 * @return boolean
+	 * @return boolean 
 	 */
-	private boolean keepIterating() {
-		boolean i = false;
+	public boolean keepIterating() {
+		boolean b = false;
 		if (this.timer == 0) {
-			i = iterator < 1;
+			b = iterator < 1;
 			iterator++;
 		} else {
-			i = System.currentTimeMillis() - this.startTime <= this.timer;
+			b = System.currentTimeMillis() - this.startTime <= this.timer;
 		}
-		return i;
+		return b;
 	}
 
 	/**
 	 * start the subscribe loop
 	 * 
-	 * @return ConsumerGeneric instance (is that realy needed??)
+	 * @return This ConsumerGeneric (to allow chaining) (is that realy needed??)
 	 * @throws Exception - generic Exception
 	 */
-	private ConsumerGeneric<V> start() throws Exception {
+	public ConsumerGeneric<V> start() throws Exception {
 		startTime = System.currentTimeMillis();
 		// Long startTime = System.currentTimeMillis();
+		cf.print("=== ConsumerGeneric.start ===");
 		cf.print(startTime.toString());
 		cf.print("Timer " + this.timer);
-		cf.print(LocalDateTime.now().toString());
+		cf.print("Start Iterator: "+LocalDateTime.now().toString());
 		while (keepIterating()) {
 			ConsumerRecords<String, V> records = this.kafkaConsumer.poll(Duration.ofMillis(1000));
 			for (ConsumerRecord<String, V> record : records) {
+				//cf.print("debug-for1");
 				this.processData(record.value().toString());
+				//cf.print("debug-for2");
 				if (this.doCommit) {
 					kafkaConsumer.commitAsync();
 				}
+				//cf.print("debug-for3");
 			}
 		}
-		cf.print(LocalDateTime.now().toString());
+		cf.print("End Iterator: "+LocalDateTime.now().toString());
 		kafkaConsumer.close();
 		cf.closeJdbcConnection();
 		return this;
@@ -103,13 +107,14 @@ public class ConsumerGeneric<V> {
 	 * - in scope of subscribing on a topic, it'll run for 1 day. a respawner (like
 	 * rmj) should keep it running in a forever loop.<br>
 	 * 
-	 * @return Consumer instance of this class
+	 * @return This ConsumerGeneric (to allow chaining)
 	 * @throws Exception generic exception (should not crash)
 	 */
 	public ConsumerGeneric<V> subscribe() throws Exception {
 		this.doCommit = true;
 		kafkaConsumer.subscribe(Arrays.asList(cf.getTopic()));
-		return start();
+		start();
+		return this;
 	}
 
 	/**
@@ -139,7 +144,7 @@ public class ConsumerGeneric<V> {
 	/**
 	 * Set the timer (how long should the subscriber loop before it stops
 	 * 
-	 * @return instance of ConsumerGeneric
+	 * @return This ConsumerGeneric (to allow chaining)
 	 * @param t typeTimer
 	 */
 	public ConsumerGeneric<V> setTimer(typeTimer t) {
@@ -167,7 +172,7 @@ public class ConsumerGeneric<V> {
 	 * Start a seek process on an instance of a consumer<br>
 	 * (a subscriber that stops after 1 itteration).
 	 * 
-	 * @return Consumer instance of class
+	 * @return This ConsumerGeneric (to allow chaining)
 	 * @throws Exception generic exception
 	 */
 	public ConsumerGeneric<V> seek() throws Exception {
@@ -176,14 +181,15 @@ public class ConsumerGeneric<V> {
 		TopicPartition topicPartition = new TopicPartition(cf.getTopic(), Integer.valueOf(cf.getPartition()));
 		kafkaConsumer.assign(Arrays.asList(topicPartition));
 		kafkaConsumer.seek(topicPartition, Long.valueOf(cf.getOffset()));
-
-		return start();
+    start();
+		return this;
 	}
 	
 	/**
 	 * Print a list of topics to the PrintWriter<br>
 	 * Just instantiate a Consumer and the call this method:<br>
-	 * new Consumer(kafkaClientFactory).printTopics();
+	 * new Consumer(kafkaClientFactory).printTopics();<br>
+	 * <em>do not forget to initialize a printwriter in the kafkaFactory</em> :-)
 	 */
 	public void printTopics() {
 	    Map<String, List<PartitionInfo>> m = this.kafkaConsumer.listTopics();
@@ -199,8 +205,9 @@ public class ConsumerGeneric<V> {
 	 * @throws Exception generic exception
 	 */
 	public void processData(String value) throws Exception {
+		LocalDateTime start = LocalDateTime.now();
+		cf.print("startTime Processing: "+start.toString());		
 		if (cf.jdbcConnection() != null && KafkaUtil.isNotBlank(cf.getJdbcQuery())) {
-	
 			try(CallableStatement stmt = cf.jdbcConnection().prepareCall("{ call " + cf.getJdbcQuery() + " }")){
 				stmt.setString(1, value);
 				stmt.execute();
@@ -214,6 +221,9 @@ public class ConsumerGeneric<V> {
 				}
 			}
 		}
+		LocalDateTime end = LocalDateTime.now();
+		cf.print("endTime Processing: "+end.toString());
+		cf.print("duration Processing: "+Duration.between(start,end).toMillis());
 
 		if (this.doPrintValues) {
 			cf.print(value);
